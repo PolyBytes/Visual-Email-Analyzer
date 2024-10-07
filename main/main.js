499eeca0e32a62bbfa55ab0a68e16b68f951d18e
PolyBytes <polybytes@proton.me>
2024-10-07 16:10:31 -0400

    Added folder selection and total message viewing functionality
    - When the user selects an account, it will populate an additional drop down menu with all folders in that account.
    - When the user selects a folder, it will display the total number of messages in that folder.
    - Refactored some function names and variable names to be more intuitive.
    - Refactored some of the code in the populateAccountSelection function to be more readable.
    - Again, this rudimentary functionality is in preparation for the end goal of the application as well as me learning how to use the Thunderbird extension API.

--- main/main.js
+++ main/main.js
@@ -1,26 +1,61 @@
-async function refreshAccounts() {
-	var accounts = await messenger.accounts.list();
-
+function attachEventHandlers()
+{
 	$('#account-selection').on('change', onAccountSelected);
+	$('#folder-selection').on('change', onFolderSelected);
+}
+
+async function populateAccountSelection() {
+	let accounts = await messenger.accounts.list();
+
+	$('#account-selection').empty();
+	$('#account-selection').append(createDefaultOption('Select an Account'));
 
 	for (account of accounts) {
-		$('#account-selection').append(
-			$('<option>', {
-				text: account.name,
-				value: account.id
-			})
-		);
+		let newAccountOption = $('<option>', {
+			text: account.name,
+			value: account.id
+		});
+
+		$('#account-selection').append(newAccountOption);
 	}
 }
 
 async function onAccountSelected() {
+	$('#folder-information-panel').hide();
+
 	let accountID = $('#account-selection').val();
 	let account = await messenger.accounts.get(accountID);
 	let rootFolder = await messenger.folders.get(account.rootFolder.id, true);
-	let folders = getAllFolders(rootFolder.subFolders);
+	let accountFolders = getAllFolders(rootFolder.subFolders);
 
 	$('#account-name').text(account.name);
-	$('#account-folder-count').text(folders.length);
+	$('#account-folder-count').text(accountFolders.length);
+
+	populateFolderSelection(accountFolders);
+
+	$('#folder-selection-panel').show();
+}
+
+function populateFolderSelection(accountFolders) {
+	$('#folder-selection').empty();
+	$('#folder-selection').append(createDefaultOption('Select a Folder'));
+
+	for (folder of accountFolders) {
+		let newFolderOption = $('<option>', {
+			text: folder.name,
+			value: folder.id
+		});
+
+		$('#folder-selection').append(newFolderOption);
+	}
+}
+
+async function onFolderSelected() {
+	let folderID = $('#folder-selection').val();
+	let folderInfo = await messenger.folders.getFolderInfo(folderID);
+
+	$('#folder-information-panel').show();
+	$('#folder-message-count').text(folderInfo.totalMessageCount);
 }
 
 // Recursively generate an array containing all folders
@@ -39,6 +74,26 @@ function getAllFolders(rootFolders) {
 	return folders;
 }
 
-$(document).ready(() => {
-	refreshAccounts();
+// Returns an <option> element which is primarily used in <select> elements
+// to inform the user they need to choose an option in the drop down list.
+function createDefaultOption(displayedText)
+{
+	let option = $('<option>', {
+		text: displayedText,
+		value: ''
+	});
+
+	option.prop({
+		'selected': true,
+		'disabled': true,
+		'hidden': true
+	});
+
+	return option;
+}
+
+// Called when the page has finished loading and is ready
+$(function() {
+	attachEventHandlers();
+	populateAccountSelection();
 });
